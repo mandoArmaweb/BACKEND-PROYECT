da83c95c70890d766f7dddc3ba0ad1a51cd510bb
marvin alejandro pichardo saucedo <pichardosaucedojesusjesus@gmail.com>
2022-05-24 02:26:14 -0500

    he editado la parte de el encabezado

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express()
 
-app.get('/', (req, res) => res.send("Hello World"))
+// app.get('/', (req, res) => res.send("Hello World"))
+
+app.use(express.static('public'))
 
 app.listen(process.env.PORT || 3000)
 console.log('server on port', process.env.PORT || 3000);
