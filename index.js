9ce6e0638537fb977e4679012c7c0296173aae3b
marvin alejandro pichardo saucedo <pichardosaucedojesusjesus@gmail.com>
2022-05-24 03:02:56 -0500

    template engine added & rest api

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const path = require('path');
+const ejs = require('ejs')
 
 const app = express()
 
-// app.get('/', (req, res) => res.send("Hello World"))
+//Setings
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
+// routes
+app.get('/api/users', (req, res) => res.json([{name: 'ryan'}, {name: "joe"}]))
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {
+        name: 'Jesus Armando pichardo saucedo',
+        age: 26
+    })
+})
+
+// static files
 app.use(express.static('public'))
 
 app.listen(process.env.PORT || 3000)
