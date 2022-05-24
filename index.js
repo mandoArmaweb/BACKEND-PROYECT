1f028d7fc1a61979a0f40c9232f69fb530192596
marvin alejandro pichardo saucedo <pichardosaucedojesusjesus@gmail.com>
2022-05-24 01:47:37 -0500

    mi primer hello world

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express()
 
-app.get('/api', (req, res) => res.send("Hello World"))
+app.get('/', (req, res) => res.send("Hello World"))
 
 app.listen(process.env.PORT || 3000)
 console.log('server on port', process.env.PORT || 3000);
